b37238c8a746d6aaf8596186e71883f4f5501949
Michal Špaček <tupinek@gmail.com>
2015-10-07 16:38:46 +0200

    Add EXAMPLE4 to doc.

--- Novosibirsk.pm
+++ Novosibirsk.pm
@@ -197,6 +197,29 @@ For more information about Novosibirsk Map, click L<here|https://en.wikipedia.or
 
 =end html
 
+=head1 EXAMPLE4
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Encode qw(encode_utf8);
+ use Map::Tube::Novosibirsk;
+
+ # Object.
+ my $obj = Map::Tube::Novosibirsk->new;
+
+ # Get lines.
+ my $lines_ar = $obj->get_lines;
+
+ # Print out.
+ map { print encode_utf8($_->name)."\n"; } sort @{$lines_ar};
+
+ # Output:
+ # Дзержинская линия
+ # Ленинская линия
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
