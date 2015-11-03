d07446d4525284ea17d83e8e68b511c2c391b071
Michal Špaček <tupinek@gmail.com>
2015-11-03 23:19:55 +0100

    Add EXAMPLE5 section to doc.

--- Novosibirsk.pm
+++ Novosibirsk.pm
@@ -220,6 +220,48 @@ For more information about Novosibirsk Map, click L<here|https://en.wikipedia.or
  # Дзержинская линия
  # Ленинская линия
 
+=head1 EXAMPLE5
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Encode qw(decode_utf8 encode_utf8);
+ use Map::Tube::Novosibirsk;
+
+ # Arguments.
+ if (@ARGV < 1) {
+         print STDERR "Usage: $0 line\n";
+         exit 1;
+ }
+ my $line = decode_utf8($ARGV[0]);
+
+ # Object.
+ my $obj = Map::Tube::Novosibirsk->new;
+
+ # Get stations for line.
+ my $stations_ar = $obj->get_stations($line);
+
+ # Print out.
+ map { print encode_utf8($_->name)."\n"; } @{$stations_ar};
+
+ # Output:
+ # Usage: __PROG__ line
+
+ # Output with 'foo' argument.
+ # Map::Tube::get_stations(): ERROR: Invalid Line Name [foo]. (status: 105) file __PROG__ on line __LINE__
+
+ # Output with 'Ленинская линия' argument.
+ # Заельцовская
+ # Гагаринская
+ # Красный проспект
+ # Площадь Ленина
+ # Октябрьская
+ # Речной вокзал
+ # Студенческая
+ # Площадь Маркса
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
